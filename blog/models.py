5d81d07ebffde7e6b18c617de74901d692de173a
Mathieu Gerard <mathieu.gerard@uclouvain.be>
2016-03-16 14:38:31 +0100

    DG app - initial commit

---
+++ blog/models.py
@@ -0,0 +1,26 @@
+from django.db import models
+from django.utils import timezone
+
+
+class Post(models.Model):
+    author = models.ForeignKey('auth.User')
+    category = models.ForeignKey('Category')
+    title = models.CharField(max_length=200)
+    text = models.TextField()
+    created_date = models.DateTimeField(
+            default=timezone.now)
+    published_date = models.DateTimeField(
+            blank=True, null=True)
+
+    def publish(self):
+        self.published_date = timezone.now()
+        self.save()
+
+    def __str__(self):
+        return self.title
+
+class Category(models.Model):
+    title = models.CharField(max_length=200)
+
+    def __str__(self):
+        return self.title
